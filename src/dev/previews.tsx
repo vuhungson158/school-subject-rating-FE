c1d7d8995ed6d55a83bbdea661a41d05bce6b85b
vuhungson <vuhungson158@gmail.com>
2024-01-23 22:35:08 +0900

    dev folder fix error

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
+import {Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
-import Table, {Filter, ShowColumnController} from "../features/subject/base/Table";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
-            <ComponentPreview path="/Table">
-                <Table/>
-            </ComponentPreview>
-            <ComponentPreview path="/Filter">
-                <Filter/>
-            </ComponentPreview>
-            <ComponentPreview path="/ShowColumnController">
-                <ShowColumnController/>
-            </ComponentPreview>
+
         </Previews>
     );
 };
